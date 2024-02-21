ae3755752c6ae892aaa33618ee974a8f476b2dc0
Tarmo90 <160609842+Tarmo90@users.noreply.github.com>
2024-02-21 20:13:57 +0200

    Kodutoo1

---
+++ kodutoo/src/App.js
@@ -0,0 +1,58 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <button className="nupp">Vajuta siia</button>
+
+    <div className="tekst kaldkiri">Tere tulemast</div>
+    <div className="roheline">Roheline tekst</div>
+
+    <img className="pilt" src="https://img.pikbest.com/origin/09/24/39/41RpIkbEsTxDz.jpg!w700wp" alt='Bitcoini logo'/>
+    
+    <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY" title='Funny' />
+
+
+    <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+    </div>
+  );
+}
+
+export default App;
