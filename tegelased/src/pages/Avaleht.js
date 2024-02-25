f04fc5bd4da8f3ba6748fcb3e02540c1e9e98bda
Tarmo90 <160609842+Tarmo90@users.noreply.github.com>
2024-02-25 16:55:54 +0200

    Lisatud LisaTegelane komponent
    Lisati LisaTegelane komponent, mis võimaldab kasutajal sisestada tegelase nime ja lisada seejärel uue tegelase.

---
+++ tegelased/src/pages/Avaleht.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Avaleht() {
+  return (
+    <>
+      <div>
+        <div>Mickey</div>
+        <div>Mouse</div>
+        <div>Disneyland</div>
+      </div>
+      <div>
+        <div>Minnie</div>
+        <div>Mouse</div>
+        <div>Disneyland</div>
+      </div>
+      <div>
+        <div>Winnie</div>
+        <div>Pooh</div>
+        <div>Hundred Acre Wood</div>
+      </div>
+      <div>
+        <div>Roo</div>
+        <div>Kangaroo</div>
+        <div>Hundred Acre Wood</div>
+      </div>
+      <div>
+        <div>Scooby</div>
+        <div>Doo</div>
+        <div>Crystal Cove</div>
+      </div>
+    </>
+  );
+}
+
+export default Avaleht;
+
